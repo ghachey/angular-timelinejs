18eb14b481b44c57adbde4c07fc324bd4dc600ce
ghachey <ghachey@gmail.com>
2014-04-21 15:25:19 +0800

    Created basic angular application to hold demo of directive.

---
+++ demo/angular-timelinejs-demo/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularTimelinejsDemoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
