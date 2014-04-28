002f17e5d86da508e9845613545761beed1d082a
ghachey <ghachey@gmail.com>
2014-04-28 09:13:24 +0800

    Pushed some of the timelinejs configuration to the directive's isolate scope

--- src/app/scripts/directives/pipptimelinejs.js
+++ src/app/scripts/directives/pipptimelinejs.js
@@ -10,22 +10,59 @@ angular.module('pippTimelineDirectives', [])
   return {
     template: '<div id="pipp-timeline"></div>',
     restrict: 'E',
-    scope: {timelineData: '=sourceData'},
+    scope: {
+      source: '=',
+      width: '@',
+      height: '@',
+      start_zoom_adjust: '@',
+      start_at_end: '@',
+      start_at_slide: '@',
+      hash_bookmark: '@',
+      font: '@',
+      lang: '@',
+      debug: '@'
+    },
     link: function postLink(scope, iElement, iAttrs) {
 
+      // Required options
       var timeline_conf = {
-        type: 'timeline', // What are other types? not documented in TimelineJS
-        embed_id: 'pipp-timeline', // is this used?
-        width: '400px', // Ignored at the moment?
-        height: '200px', // Ignored at the moment?
-        source: scope.timelineData, // working
-        embed: false, // Effect?
-        start_zoom_adjust: '0', // working
-        start_at_slide: '0', // working
-        hash_bookmark: false, // working, how to integrate with Angular routing?!
-        debug: false // seems to debug no matter what?
+        source: scope.source,
+
+        // Ignored at the moment, why?
+        width: (scope.width === undefined) ? '100%' : scope.width,
+
+        // Ignored at the moment, why?
+        height: (scope.height === undefined) ? '400' : scope.height,
       };
 
+      // Optional options
+
+      // What are other types? not documented in TimelineJS
+      // Not yet available for change to user
+      if (scope.type) timeline_conf[type] = scope.type;
+
+      // is this used? First glance did not see effect of change
+      // Not yet available for change to user
+      if (scope.embed_id) timeline_conf[embed_id] = scope.embed_id;
+
+      // First glance did not see the effect?
+      // Not yet available for change to user
+      if (scope.embed) timeline_conf[embed] = scope.embed;
+
+      if (scope.start_at_zoom) timeline_conf[start_at_zoom] = scope.start_at_zoom;
+      if (scope.start_at_slide) timeline_conf[start_at_slide] = scope.start_at_slide;
+
+      // working, but how to integrate with Angular routing?! Something to ponder
+      if (scope.hash_bookmark) timeline_conf[hash_bookmark] = scope.hash_bookmark;
+
+      if (scope.font) timeline_conf[font] = scope.font;
+
+      // seems to debug no matter what?
+      if (scope.debug) timeline_conf[debug] = scope.debug;
+
+      console.log("CONFIG: ", JSON.stringify(timeline_conf));
+
+      // Instantiate timeline object and manipulate DOM
       var timeline = new VMM.Timeline('pipp-timeline');
       timeline.init(timeline_conf);
 
